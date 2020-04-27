bddaed7e3b6e70b0c8576557200da5f3ff9aaf62
Alex <smileyrec@gmail.com>
2020-04-27 11:50:54 +0200

    Updates

---
+++ precache-manifest.64e454bbee5eb34361d1909646195b4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3a39620dd9b9494fe708c7fb8bf5ce",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fbe510d8c614489d681a",
+    "url": "/monsters-rolodex/static/css/main.af448832.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe510d8c614489d681a",
+    "url": "/monsters-rolodex/static/js/main.9b0248a0.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
